6e95e6b7bd7af0781d584b0713b9b38d5f24eb2e
Joanne Candia <jcusa2@gmail.com>
2026-07-06 20:35:24 -0400

    Added Files
    Front End:  User Log in Screen, User Dashboard/Portal, Ticket Submission (used Ethan's coding ticket creation for this)
    
    NOTE: Did not link the "TicketFlowUser" information to this.  So tickets are still held in local storage instead of a repostory.

---
+++ TicketFlow - JC - User Front End + Ticket/Ticket.js
@@ -0,0 +1,47 @@
+class Ticket {
+
+    constructor(id, title, description, category, department, priority, expectedDate) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.category = category;
+        this.department = department;
+        this.priority = priority;
+        this.expectedDate = expectedDate;
+
+        this.status = "Open";
+
+        this.createdDate = new Date();
+        this.closedDate = null;
+
+        this.technicianNotes = "";
+        this.notes = [];
+
+        this.activityLog = [];
+
+        this.addActivity("Ticket created.");
+    }
+
+    addActivity(message) {
+        this.activityLog.push({
+            date: new Date(),
+            message: message
+        });
+    }
+
+    updateStatus(status) {
+        this.status = status;
+
+        this.addActivity(`Status changed to ${status}`);
+
+        if (status === "Closed") {
+            this.closedDate = new Date();
+        }
+    }
+
+    addTechnicianNote(note) {
+        this.technicianNotes += note + "\n";
+        this.addActivity("Technician note added.");
+    }
+
+}
